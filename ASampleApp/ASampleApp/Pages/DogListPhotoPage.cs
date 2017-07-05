a3ebc1dff70493d1ff2660e487f752f6b216b735
ac <andrew.chung@xamarin.com>
2017-07-04 21:16:25 -0700

    Muted Out Dog List Photo Page

--- ASampleApp/ASampleApp/Pages/DogListPhotoPage.cs
+++ ASampleApp/ASampleApp/Pages/DogListPhotoPage.cs
@@ -1,128 +1,128 @@
-﻿using System;
-using Xamarin.Forms;
+﻿//using System;
+//using Xamarin.Forms;
 
-namespace ASampleApp
-{
-    public class DogListPhotoPage : BaseContentPage<DogListPhotoViewModel>
-	{
-		Label _firstLabel;
-		Entry _firstEntry;
-		Entry _secondEntry;
-		Button _firstButton;
-		Button _goToDogListButton;
+//namespace ASampleApp
+//{
+//    public class DogListPhotoPage : BaseContentPage<DogListPhotoViewModel>
+//	{
+//		Label _firstLabel;
+//		Entry _firstEntry;
+//		Entry _secondEntry;
+//		Button _firstButton;
+//		Button _goToDogListButton;
 
-		Button _addAddDogPhotoURLButton;
-		Button _addAddDogPhotoButton;
-		Button _goToDogPhotoListButton;
-
-
-		public DogListPhotoPage ()
-		{
-
-			this.Title = "Dog List Photo Page";
+//		Button _addAddDogPhotoURLButton;
+//		Button _addAddDogPhotoButton;
+//		Button _goToDogPhotoListButton;
 
 
-			//METHOD#1 non-MVVM
-			//BindingContext = new FirstViewModel();
+//		public DogListPhotoPage ()
+//		{
 
-			_firstLabel = new Label(); //{ Text = "Hello"};
-			_firstEntry = new Entry() { Placeholder = "Dog Name" };
-			_secondEntry = new Entry() { Placeholder = "Fur color" };
-			_firstButton = new Button() { Text = "Button" };
-			_goToDogListButton = new Button() { Text = "Go to Dog List" };
-
-			_addAddDogPhotoButton = new Button() { Text = "Add Dog Photo" };
-			_addAddDogPhotoURLButton = new Button { Text = "Add Dog Photo URL" };
-			_goToDogPhotoListButton = new Button() { Text = "Go to Dog Photo List" };
+//			this.Title = "Dog List Photo Page";
 
 
-			//METHOD#2 MVVM
-			//
-			_firstLabel.SetBinding(Label.TextProperty, nameof(MyViewModel.FirstLabel), BindingMode.TwoWay);
-			_firstEntry.SetBinding(Entry.TextProperty, nameof(MyViewModel.FirstEntryText));
-			_secondEntry.SetBinding(Entry.TextProperty, nameof(MyViewModel.SecondEntryText));
+//			//METHOD#1 non-MVVM
+//			//BindingContext = new FirstViewModel();
 
-			_firstButton.SetBinding(Button.CommandProperty, nameof(MyViewModel.MyFavoriteCommand));
+//			_firstLabel = new Label(); //{ Text = "Hello"};
+//			_firstEntry = new Entry() { Placeholder = "Dog Name" };
+//			_secondEntry = new Entry() { Placeholder = "Fur color" };
+//			_firstButton = new Button() { Text = "Button" };
+//			_goToDogListButton = new Button() { Text = "Go to Dog List" };
 
-
-			Content = new StackLayout
-			{
-				Margin = 20,
-				Children =
-				{
-					_firstLabel,
-					_firstEntry,
-					_secondEntry,
-					_firstButton,
-					_goToDogListButton,
-					_addAddDogPhotoButton,
-					_addAddDogPhotoURLButton,
-					_goToDogPhotoListButton
-
-				}
-
-			};
-
-		}
+//			_addAddDogPhotoButton = new Button() { Text = "Add Dog Photo" };
+//			_addAddDogPhotoURLButton = new Button { Text = "Add Dog Photo URL" };
+//			_goToDogPhotoListButton = new Button() { Text = "Go to Dog Photo List" };
 
 
+//			//METHOD#2 MVVM
+//			//
+//			_firstLabel.SetBinding(Label.TextProperty, nameof(MyViewModel.FirstLabel), BindingMode.TwoWay);
+//			_firstEntry.SetBinding(Entry.TextProperty, nameof(MyViewModel.FirstEntryText));
+//			_secondEntry.SetBinding(Entry.TextProperty, nameof(MyViewModel.SecondEntryText));
 
-		protected override void OnAppearing()
-		{
-			base.OnAppearing();
-			//METHOD 1
-			//          _firstButton.Clicked += OnFirstButtonClicked;
-
-			_goToDogListButton.Clicked += OnToDogListClicked;
-
-			//TEST BY ADDING DOGS ON EACH ONAPPEARING
-			//App.DogRep.AddNewDog("Oliver", "Black");
-			//App.DogRep.AddNewDog("Oliver", "Black");
+//			_firstButton.SetBinding(Button.CommandProperty, nameof(MyViewModel.MyFavoriteCommand));
 
 
-		}
+//			Content = new StackLayout
+//			{
+//				Margin = 20,
+//				Children =
+//				{
+//					_firstLabel,
+//					_firstEntry,
+//					_secondEntry,
+//					_firstButton,
+//					_goToDogListButton,
+//					_addAddDogPhotoButton,
+//					_addAddDogPhotoURLButton,
+//					_goToDogPhotoListButton
 
-		protected override void OnDisappearing()
-		{
-			base.OnDisappearing();
-			//METHOD 1
+//				}
 
-			//          _firstButton.Clicked -= OnFirstButtonClicked;
+//			};
 
-			_goToDogListButton.Clicked -= OnToDogListClicked;
-		}
-
-		void OnToDogListClicked(object sender, EventArgs e)
-		{
-			//OPTION 1
-			//Device.BeginInvokeOnMainThread (() => Navigation.PushAsync (new DogListPage ()));
-
-			//OPTION 2
-			Device.BeginInvokeOnMainThread(() => Navigation.PushAsync(new DogListMVVMPage()));
-		}
+//		}
 
 
 
-		//EventHandler OnToDogListClicked ()
-		//{
-		//  //throw new NotImplementedException ();
-		//  Device.BeginInvokeOnMainThread (()=> Navigation.PushAsync (new DogListPage()));
-		//}
+//		protected override void OnAppearing()
+//		{
+//			base.OnAppearing();
+//			//METHOD 1
+//			//          _firstButton.Clicked += OnFirstButtonClicked;
 
-		//void OnFirstButtonClicked (object sender, EventArgs e)
-		//{
-		//  Console.WriteLine ("Hello there;");
-		//  string ft = _firstEntry.Text;
+//			_goToDogListButton.Clicked += OnToDogListClicked;
 
-		//  //SIMPLE ACTION 1
-		//  //Device.BeginInvokeOnMainThread (() => 
-		//  //                                _firstLabel.Text = ft
-		//  //                               );
+//			//TEST BY ADDING DOGS ON EACH ONAPPEARING
+//			//App.DogRep.AddNewDog("Oliver", "Black");
+//			//App.DogRep.AddNewDog("Oliver", "Black");
 
-		//  //SIMPLE ACTION 2
-		//  //ViewModel.FirstLabel = "hi there!";
 
-		//}
-	}
-}
+//		}
+
+//		protected override void OnDisappearing()
+//		{
+//			base.OnDisappearing();
+//			//METHOD 1
+
+//			//          _firstButton.Clicked -= OnFirstButtonClicked;
+
+//			_goToDogListButton.Clicked -= OnToDogListClicked;
+//		}
+
+//		void OnToDogListClicked(object sender, EventArgs e)
+//		{
+//			//OPTION 1
+//			//Device.BeginInvokeOnMainThread (() => Navigation.PushAsync (new DogListPage ()));
+
+//			//OPTION 2
+//			Device.BeginInvokeOnMainThread(() => Navigation.PushAsync(new DogListMVVMPage()));
+//		}
+
+
+
+//		//EventHandler OnToDogListClicked ()
+//		//{
+//		//  //throw new NotImplementedException ();
+//		//  Device.BeginInvokeOnMainThread (()=> Navigation.PushAsync (new DogListPage()));
+//		//}
+
+//		//void OnFirstButtonClicked (object sender, EventArgs e)
+//		//{
+//		//  Console.WriteLine ("Hello there;");
+//		//  string ft = _firstEntry.Text;
+
+//		//  //SIMPLE ACTION 1
+//		//  //Device.BeginInvokeOnMainThread (() => 
+//		//  //                                _firstLabel.Text = ft
+//		//  //                               );
+
+//		//  //SIMPLE ACTION 2
+//		//  //ViewModel.FirstLabel = "hi there!";
+
+//		//}
+//	}
+//}
 
--- ASampleApp/ASampleApp/ViewModels/DogListPhotoViewModel.cs
+++ ASampleApp/ASampleApp/ViewModels/DogListPhotoViewModel.cs
@@ -1,64 +1,89 @@
-﻿using System;
-using System.Windows.Input;
-using Xamarin.Forms;
-
-namespace ASampleApp
-{
-	public class DogListPhotoViewModel : BaseViewModel
-	{
-
-		string _firstLabel;
-		string _firstEntryText;
-		string _secondEntryText;
-
-		public ICommand MyFavoriteCommand { get; set; }
-		public ICommand MySecondFavoriteCommand { get; set; }
-
-
-		public string FirstLabel
-		{
-			get { return _firstLabel; }
-			set { SetProperty(ref _firstLabel, value); }
-		}
+﻿
 
 
 
-		public string FirstEntryText
-		{
-			get { return _firstEntryText; }
-			set { SetProperty(ref _firstEntryText, value); }
-		}
-
-		public string SecondEntryText
-		{
-			get { return _secondEntryText; }
-			set { SetProperty(ref _secondEntryText, value); }
-		}
 
 
-		public DogListPhotoViewModel()
-		{
-			MyFavoriteCommand = new Command(OnMyFavoriteAction);
-			//MySecondFavoriteCommand = new Command(async () => await OnMySecondFavoriteCommand());
-
-		}
-
-		void OnMyFavoriteAction()
-		{
 
 
-			//RETURN DOG
-			App.DogRep.AddNewDog(this.FirstEntryText, this.SecondEntryText);
-			string _lastNameString = App.DogRep.GetLastDog().Name;
 
 
-			string _lastNameStringAdd = System.String.Format("{0} added to the list!", _lastNameString);
-			this.FirstLabel = _lastNameStringAdd;
-
-			return;
-
-		}
 
 
-	}
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+//using System;
+//using System.Windows.Input;
+//using Xamarin.Forms;
+
+//namespace ASampleApp
+//{
+//	public class DogListPhotoViewModel : BaseViewModel
+//	{
+
+//		string _firstLabel;
+//		string _firstEntryText;
+//		string _secondEntryText;
+
+//		public ICommand MyFavoriteCommand { get; set; }
+//		public ICommand MySecondFavoriteCommand { get; set; }
+
+
+//		public string FirstLabel
+//		{
+//			get { return _firstLabel; }
+//			set { SetProperty(ref _firstLabel, value); }
+//		}
+
+
+
+//		public string FirstEntryText
+//		{
+//			get { return _firstEntryText; }
+//			set { SetProperty(ref _firstEntryText, value); }
+//		}
+
+//		public string SecondEntryText
+//		{
+//			get { return _secondEntryText; }
+//			set { SetProperty(ref _secondEntryText, value); }
+//		}
+
+
+//		public DogListPhotoViewModel()
+//		{
+//			MyFavoriteCommand = new Command(OnMyFavoriteAction);
+//			//MySecondFavoriteCommand = new Command(async () => await OnMySecondFavoriteCommand());
+
+//		}
+
+//		void OnMyFavoriteAction()
+//		{
+
+
+//			//RETURN DOG
+//			App.DogRep.AddNewDog(this.FirstEntryText, this.SecondEntryText);
+//			string _lastNameString = App.DogRep.GetLastDog().Name;
+
+
+//			string _lastNameStringAdd = System.String.Format("{0} added to the list!", _lastNameString);
+//			this.FirstLabel = _lastNameStringAdd;
+
+//			return;
+
+//		}
+
+
+//	}
+//}
